70373e7b0475305f6c68f581a7469666df1e4163
Christoph Grabo <asaaki@mannaz.cc>
2016-01-18 15:12:47 +0100

    Configure dogma

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,11 @@ use Mix.Config
 if Mix.env == :docs do
   config :ex_doc, :markdown_processor, ExDoc.Markdown.Cmark
 end
+
+if Mix.env == :lint do
+  config :dogma,
+    rule_set: Dogma.RuleSet.All,
+    override: %{
+      LineLength => [max_length: 120],
+    }
+end
