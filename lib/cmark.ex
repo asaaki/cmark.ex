aadaaab930ab3e2ac0169d5db481ee2f2bcb60e3
Christoph Grabo <asaaki@mannaz.cc>
2016-01-23 17:05:20 +0100

    Adjust docs around options list

--- lib/cmark.ex
+++ lib/cmark.ex
@@ -57,30 +57,27 @@ defmodule Cmark do
   @typedoc "A callback function which can return anything"
   @type callback_fun :: (function -> any)
 
-  @typedoc """
-  List of atoms
-
-  For more information about options see `Cmark.to_html/2` or any other function using options.
-  """
-  @type options_list :: [atom]
-
-  @typedoc "Either an options list or a callback function"
-  @type options_or_callback :: options_list | callback_fun
-
   @options_doc """
-  Options are passed as a list of atoms. Available options are:
+  Available options are:
 
-  * `:sourcepos` - Include a `data-sourcepos` attribute on all block elements.
-  * `:softbreak` - Render `softbreak` elements as hard line breaks.
-  * `:normalize` - Normalize tree by consolidating adjacent text nodes.
-  * `:smart` - Convert straight quotes to curly, --- to em dashes, -- to en dashes.
-  * `:validate_utf8` - Validate UTF-8 in the input before parsing, replacing
+  - `:sourcepos` - Include a `data-sourcepos` attribute on all block elements.
+  - `:softbreak` - Render `softbreak` elements as hard line breaks.
+  - `:normalize` - Normalize tree by consolidating adjacent text nodes.
+  - `:smart` - Convert straight quotes to curly, --- to em dashes, -- to en dashes.
+  - `:validate_utf8` - Validate UTF-8 in the input before parsing, replacing
      illegal sequences with the replacement character U+FFFD.
-  * `:safe` - Suppress raw HTML and unsafe links (`javascript:`, `vbscript:`,
+  - `:safe` - Suppress raw HTML and unsafe links (`javascript:`, `vbscript:`,
     `file:`, and `data:`, except for `image/png`, `image/gif`, `image/jpeg`, or
     `image/webp` mime types).  Raw HTML is replaced by a placeholder HTML
     comment. Unsafe links are replaced by empty strings.
   """
+
+  @typedoc "A list of atoms describing the options to use
+            (for details check documentation of any function using options)"
+  @type options_list :: [atom]
+
+  @typedoc "Either an options list or a callback function"
+  @type options_or_callback :: options_list | callback_fun
 
   ##### HTML #####
 
