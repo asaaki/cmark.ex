a0d2d6583a4ccdc9cfb172c9c55e638e5574095f
Christoph Grabo <asaaki@mannaz.cc>
2016-07-05 02:09:13 +0200

    Remove obsolete dogma config

--- config/config.exs
+++ config/config.exs
@@ -3,11 +3,3 @@ use Mix.Config
 if Mix.env in ~w(docs ci)a do
   config :ex_doc, :markdown_processor, ExDoc.Markdown.Cmark
 end
-
-if Mix.env in ~w(lint ci)a do
-  config :dogma,
-    rule_set: Dogma.RuleSet.All,
-    override: %{
-      LineLength => [max_length: 120],
-    }
-end
