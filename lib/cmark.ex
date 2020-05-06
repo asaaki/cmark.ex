176c378bd6e485941a42499cd1e34f9b68786337
Christoph Grabo <asaaki@mannaz.cc>
2020-05-06 02:23:38 +0200

    Fix doctests

--- lib/cmark.ex
+++ lib/cmark.ex
@@ -130,12 +130,12 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "HTML is #{result}" |> String.strip end
+      iex> callback = fn (result) -> "HTML is #{result}" |> String.trim end
       iex> Cmark.to_html("test", callback)
       "HTML is <p>test</p>"
 
       iex> callback = fn (results) ->
-      iex>   Enum.map(results, &String.strip/1) |> Enum.join("<hr>")
+      iex>   Enum.map(results, &String.trim/1) |> Enum.join("<hr>")
       iex> end
       iex> Cmark.to_html(["list", "test"], callback)
       "<p>list</p><hr><p>test</p>"
@@ -162,7 +162,7 @@ defmodule Cmark do
       {:ok, "<p>Something “smart” and … —you get it!</p>\n"}
 
       iex> callback = fn (results) ->
-      iex>   Enum.map(results, &String.strip/1) |> Enum.join("<hr>")
+      iex>   Enum.map(results, &String.trim/1) |> Enum.join("<hr>")
       iex> end
       iex> Cmark.to_html(["en-dash --", "ellipsis..."], callback, [:smart])
       "<p>en-dash –</p><hr><p>ellipsis…</p>"
@@ -180,7 +180,7 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "HTML is #{result |> String.strip}" end
+      iex> callback = fn (result) -> "HTML is #{result |> String.trim}" end
       iex> Cmark.to_html_each(["list", "test"], callback)
       ["HTML is <p>list</p>", "HTML is <p>test</p>"]
 
@@ -201,7 +201,7 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "HTML is #{result |> String.strip}" end
+      iex> callback = fn (result) -> "HTML is #{result |> String.trim}" end
       iex> Cmark.to_html_each(["list --", "test..."], callback, [:smart])
       ["HTML is <p>list –</p>", "HTML is <p>test…</p>"]
 
@@ -220,17 +220,13 @@ defmodule Cmark do
   ## Examples
 
       iex> "test" |> Cmark.to_xml
-      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>test</text>\n  </paragraph>
-      </document>\n"
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">test</text>\n  </paragraph>\n</document>\n"
 
       iex> ["test 1", "test 2"] |> Cmark.to_xml
-      ["<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>test 1</text>\n  </paragraph>
-      </document>\n",
-      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>test 2</text>\n  </paragraph>
-      </document>\n"]
+      [
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">test 1</text>\n  </paragraph>\n</document>\n",
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">test 2</text>\n  </paragraph>\n</document>\n"
+      ]
 
   """
   @spec to_xml(string_or_list) :: string_or_list
@@ -249,18 +245,13 @@ defmodule Cmark do
   ## Examples
 
       iex> Cmark.to_xml(~s(Use option to enable "smart" quotes.), [:smart])
-      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>Use option to enable </text>
-          <text>“</text>\n    <text>smart</text>\n    <text>”</text>\n    <text> quotes</text>\n    <text>.</text>
-        </paragraph>\n</document>\n"
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">Use option to enable “smart” quotes.</text>\n  </paragraph>\n</document>\n"
 
       iex> Cmark.to_xml([~s(Something "smart"), ~s(em---dashed)], [:smart])
-      ["<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>Something </text>
-          <text>“</text>\n    <text>smart</text>\n    <text>”</text>\n  </paragraph>\n</document>\n",
-      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>em</text>
-          <text>—</text>\n    <text>dashed</text>\n  </paragraph>\n</document>\n"]
+      [
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">Something “smart”</text>\n  </paragraph>\n</document>\n",
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">em—dashed</text>\n  </paragraph>\n</document>\n"
+      ]
 
   -----
 
@@ -268,20 +259,32 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "XML is #{result}" |> String.strip end
+      iex> callback = fn (result) -> "XML is #{result}" |> String.trim end
       iex> Cmark.to_xml("test", callback)
-      "XML is <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>test</text>\n  </paragraph>
+      "XML is <?xml version=\"1.0\" encoding=\"UTF-8\"?>
+      <!DOCTYPE document SYSTEM \"CommonMark.dtd\">
+      <document xmlns=\"http://commonmark.org/xml/1.0\">
+        <paragraph>
+          <text xml:space=\"preserve\">test</text>
+        </paragraph>
       </document>"
 
       iex> callback = fn (results) ->
-      iex>   Enum.map(results, &String.strip/1) |> Enum.join("<joiner>")
+      iex>   Enum.map(results, &String.trim/1) |> Enum.join("<joiner>")
       iex> end
       iex> Cmark.to_xml(["list", "test"], callback)
-      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>list</text>\n  </paragraph>
-      </document><joiner><?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>test</text>\n  </paragraph>
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+      <!DOCTYPE document SYSTEM \"CommonMark.dtd\">
+      <document xmlns=\"http://commonmark.org/xml/1.0\">
+        <paragraph>
+          <text xml:space=\"preserve\">list</text>
+        </paragraph>
+      </document><joiner><?xml version=\"1.0\" encoding=\"UTF-8\"?>
+      <!DOCTYPE document SYSTEM \"CommonMark.dtd\">
+      <document xmlns=\"http://commonmark.org/xml/1.0\">
+        <paragraph>
+          <text xml:space=\"preserve\">test</text>
+        </paragraph>
       </document>"
 
   """
@@ -303,22 +306,16 @@ defmodule Cmark do
 
       iex> callback = fn (result) -> {:ok, result} end
       iex> Cmark.to_xml(~s(Something "smart" and ... ---you get it!), callback, [:smart])
-      {:ok, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>Something </text>
-          <text>“</text>\n    <text>smart</text>\n    <text>”</text>\n    <text> and </text>\n    <text>…</text>
-          <text> </text>\n    <text>—</text>\n    <text>you get it</text>\n    <text>!</text>\n  </paragraph>
-      </document>\n"}
+      {
+        :ok,
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">Something “smart” and … —you get it!</text>\n  </paragraph>\n</document>\n"
+      }
 
       iex> callback = fn (results) ->
-      iex>   Enum.map(results, &String.strip/1) |> Enum.join("<joiner>")
+      iex>   Enum.map(results, &String.trim/1) |> Enum.join("<joiner>")
       iex> end
       iex> Cmark.to_xml(["en-dash --", "ellipsis..."], callback, [:smart])
-      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>en</text>\n    <text>-</text>
-          <text>dash </text>\n    <text>–</text>\n  </paragraph>
-      </document><joiner><?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>ellipsis</text>\n    <text>…</text>
-        </paragraph>\n</document>"
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">en-dash –</text>\n  </paragraph>\n</document><joiner><?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">ellipsis…</text>\n  </paragraph>\n</document>"
 
   """
   @spec to_xml(string_or_list, callback_fun, options_list) :: string_or_list
@@ -333,14 +330,12 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "XML is #{result |> String.strip}" end
+      iex> callback = fn (result) -> "XML is #{result |> String.trim}" end
       iex> Cmark.to_xml_each(["list", "test"], callback)
-      ["XML is <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>list</text>\n  </paragraph>
-      </document>",
-      "XML is <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>test</text>\n  </paragraph>
-      </document>"]
+      [
+        "XML is <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">list</text>\n  </paragraph>\n</document>",
+        "XML is <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">test</text>\n  </paragraph>\n</document>"
+      ]
 
   """
   @spec to_xml_each(string_or_list, callback_fun) :: list
@@ -359,14 +354,12 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "XML is #{result |> String.strip}" end
+      iex> callback = fn (result) -> "XML is #{result |> String.trim}" end
       iex> Cmark.to_xml_each(["list --", "test..."], callback, [:smart])
-      ["XML is <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>list </text>\n    <text>–</text>
-        </paragraph>\n</document>",
-      "XML is <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">
-      <document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text>test</text>\n    <text>…</text>
-        </paragraph>\n</document>"]
+      [
+        "XML is <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">list –</text>\n  </paragraph>\n</document>",
+        "XML is <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n<document xmlns=\"http://commonmark.org/xml/1.0\">\n  <paragraph>\n    <text xml:space=\"preserve\">test…</text>\n  </paragraph>\n</document>"
+      ]
 
   """
   @spec to_xml_each(string_or_list, callback_fun, options_list) :: list
@@ -416,12 +409,12 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "Manpage is #{result}" |> String.strip end
+      iex> callback = fn (result) -> "Manpage is #{result}" |> String.trim end
       iex> Cmark.to_man("test", callback)
       "Manpage is .PP\ntest"
 
       iex> callback = fn (results) ->
-      iex>   Enum.map(results, &String.strip/1) |> Enum.join("%%joiner%%")
+      iex>   Enum.map(results, &String.trim/1) |> Enum.join("%%joiner%%")
       iex> end
       iex> Cmark.to_man(["list", "test"], callback)
       ".PP\nlist%%joiner%%.PP\ntest"
@@ -447,7 +440,7 @@ defmodule Cmark do
       {:ok, ".PP\nSomething \\[lq]smart\\[rq] and … \\[em]you get it!\n"}
 
       iex> callback = fn (results) ->
-      iex>   Enum.map(results, &String.strip/1) |> Enum.join("%%joiner%%")
+      iex>   Enum.map(results, &String.trim/1) |> Enum.join("%%joiner%%")
       iex> end
       iex> Cmark.to_man(["en-dash --", "ellipsis..."], callback, [:smart])
       ".PP\nen\\-dash \\[en]%%joiner%%.PP\nellipsis…"
@@ -465,7 +458,7 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "Manpage is #{result |> String.strip}" end
+      iex> callback = fn (result) -> "Manpage is #{result |> String.trim}" end
       iex> Cmark.to_man_each(["list", "test"], callback)
       ["Manpage is .PP\nlist", "Manpage is .PP\ntest"]
 
@@ -486,7 +479,7 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "Manpage is #{result |> String.strip}" end
+      iex> callback = fn (result) -> "Manpage is #{result |> String.trim}" end
       iex> Cmark.to_man_each(["list --", "test..."], callback, [:smart])
       ["Manpage is .PP\nlist \\[en]", "Manpage is .PP\ntest…"]
 
@@ -538,12 +531,12 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "CommonMark is #{result}" |> String.strip end
+      iex> callback = fn (result) -> "CommonMark is #{result}" |> String.trim end
       iex> Cmark.to_commonmark("test", callback)
       "CommonMark is test"
 
       iex> callback = fn (results) ->
-      iex>   Enum.map(results, &String.strip/1) |> Enum.join("%%joiner%%")
+      iex>   Enum.map(results, &String.trim/1) |> Enum.join("%%joiner%%")
       iex> end
       iex> Cmark.to_commonmark(["list", "test"], callback)
       "list%%joiner%%test"
@@ -569,7 +562,7 @@ defmodule Cmark do
       {:ok, "Something “smart” and … —you get it\\!\n"}
 
       iex> callback = fn (results) ->
-      iex>   Enum.map(results, &String.strip/1) |> Enum.join("%%joiner%%")
+      iex>   Enum.map(results, &String.trim/1) |> Enum.join("%%joiner%%")
       iex> end
       iex> Cmark.to_commonmark(["en-dash --", "ellipsis..."], callback, [:smart])
       "en-dash –%%joiner%%ellipsis…"
@@ -587,7 +580,7 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "CommonMark is #{result |> String.strip}" end
+      iex> callback = fn (result) -> "CommonMark is #{result |> String.trim}" end
       iex> Cmark.to_commonmark_each(["list", "test"], callback)
       ["CommonMark is list", "CommonMark is test"]
 
@@ -608,7 +601,7 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "CommonMark is #{result |> String.strip}" end
+      iex> callback = fn (result) -> "CommonMark is #{result |> String.trim}" end
       iex> Cmark.to_commonmark_each(["list --", "test..."], callback, [:smart])
       ["CommonMark is list –", "CommonMark is test…"]
 
@@ -660,12 +653,12 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "LaTeX is #{result}" |> String.strip end
+      iex> callback = fn (result) -> "LaTeX is #{result}" |> String.trim end
       iex> Cmark.to_latex("test", callback)
       "LaTeX is test"
 
       iex> callback = fn (results) ->
-      iex>   Enum.map(results, &String.strip/1) |> Enum.join("%%joiner%%")
+      iex>   Enum.map(results, &String.trim/1) |> Enum.join("%%joiner%%")
       iex> end
       iex> Cmark.to_latex(["list", "test"], callback)
       "list%%joiner%%test"
@@ -691,7 +684,7 @@ defmodule Cmark do
       {:ok, "Something ``smart'' and \\ldots{} ---you get it!\n"}
 
       iex> callback = fn (results) ->
-      iex>   Enum.map(results, &String.strip/1) |> Enum.join("%%joiner%%")
+      iex>   Enum.map(results, &String.trim/1) |> Enum.join("%%joiner%%")
       iex> end
       iex> Cmark.to_latex(["en-dash --", "ellipsis..."], callback, [:smart])
       "en-dash --%%joiner%%ellipsis\\ldots{}"
@@ -709,7 +702,7 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "LaTeX is #{result |> String.strip}" end
+      iex> callback = fn (result) -> "LaTeX is #{result |> String.trim}" end
       iex> Cmark.to_latex_each(["list", "test"], callback)
       ["LaTeX is list", "LaTeX is test"]
 
@@ -730,7 +723,7 @@ defmodule Cmark do
 
   ## Examples
 
-      iex> callback = fn (result) -> "LaTeX is #{result |> String.strip}" end
+      iex> callback = fn (result) -> "LaTeX is #{result |> String.trim}" end
       iex> Cmark.to_latex_each(["list --", "test..."], callback, [:smart])
       ["LaTeX is list --", "LaTeX is test\\ldots{}"]
 
