ddad20e440689845142ca487f45b4d3ec92db29a
José Valim <jose.valim@dashbit.co>
2020-05-06 16:34:31 +0200

    Simplify NIF loading

--- lib/cmark/nif.ex
+++ lib/cmark/nif.ex
@@ -5,34 +5,12 @@ defmodule Cmark.Nif do
   @doc false
   @spec init :: :ok
   def init do
-    :ok = :erlang.load_nif(nif_path(), 0)
+    path = Application.app_dir(:cmark, "priv/cmark")
+    :ok = :erlang.load_nif(String.to_charlist(path), 0)
   end
 
   @doc false
-  defp nif_path,
-    do: :filename.join(priv_dir(), 'cmark')
-
-  @doc false
-  defp priv_dir do
-    :cmark
-    |> :code.priv_dir
-    |> maybe_priv_dir
-  end
-
-  @doc false
-  defp maybe_priv_dir({:error, _}) do
-    :cmark
-    |> :code.which
-    |> :filename.dirname
-    |> :filename.dirname
-    |> :filename.join('priv')
-  end
-  defp maybe_priv_dir(path),
-    do: path
-
-  @doc false
-  @spec render(String.t, list, integer) :: String.t
-  # def render(data, options, format)
-  def render(_, _, _),
+  @spec render(String.t, integer, integer) :: String.t
+  def render(_data, _options, _format),
     do: exit(:nif_library_not_loaded)
 end
