63dda34e4d4a1173c3097fdab4edce200bb351a9
Florian Kraft <florian.kraft@contentful.com>
2020-05-06 10:38:51 +0200

    supresses a warning when running mix locally

--- mix.exs
+++ mix.exs
@@ -5,17 +5,17 @@ defmodule Cmark.Mixfile do
 
   def project do
     [
-      app:           :cmark,
-      version:       @version,
-      elixir:        "~> 1.8",
-      compilers:     [:cmark, :elixir, :app],
-      deps:          deps(),
-      package:       package(),
-      description:   description(),
-      name:          "cmark",
-      source_url:    "https://github.com/asaaki/cmark.ex",
-      homepage_url:  "http://hexdocs.pm/cmark",
-      docs:          &docs/0,
+      app: :cmark,
+      version: @version,
+      elixir: "~> 1.8",
+      compilers: [:cmark, :elixir, :app],
+      deps: deps(),
+      package: package(),
+      description: description(),
+      name: "cmark",
+      source_url: "https://github.com/asaaki/cmark.ex",
+      homepage_url: "http://hexdocs.pm/cmark",
+      docs: &docs/0,
       test_coverage: [tool: ExCoveralls]
     ]
   end
@@ -31,12 +31,12 @@ defmodule Cmark.Mixfile do
 
   defp package do
     [
-      maintainers:  ["Christoph Grabo"],
-      licenses:     ["MIT"],
+      maintainers: ["Christoph Grabo"],
+      licenses: ["MIT"],
       links: %{
         "GitHub" => "https://github.com/asaaki/cmark.ex",
         "Issues" => "https://github.com/asaaki/cmark.ex/issues",
-        "Docs"   => "http://hexdocs.pm/cmark/#{@version}/"
+        "Docs" => "http://hexdocs.pm/cmark/#{@version}/"
       },
       files: [
         "c_src/*.h",
@@ -55,8 +55,8 @@ defmodule Cmark.Mixfile do
 
   defp docs do
     [
-      extras:     ["README.md"],
-      main:       "readme",
+      extras: ["README.md"],
+      main: "readme",
       source_ref: "v#{@version}",
       source_url: "https://github.com/asaaki/cmark.ex"
     ]
@@ -69,7 +69,7 @@ defmodule Cmark.Mixfile do
       {:excoveralls, "~> 0.6", only: :ci},
       {:inch_ex, "~> 2.0", only: [:docs, :ci]},
       {:jason, "~> 1.2", only: [:dev, :test, :docs, :lint, :ci], override: true},
-      {:dialyxir, "~> 1.0", only: [:dev]},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 end
@@ -78,27 +78,31 @@ defmodule Mix.Tasks.Compile.Cmark do
   use Mix.Task
   @shortdoc "Compiles cmark library"
   def run(_) do
-    if Mix.env != :test, do: File.rm_rf("priv")
+    if Mix.env() != :test, do: File.rm_rf("priv")
     File.mkdir("priv")
 
-    make_cmd = System.get_env("MAKE") || case :os.type() do
-      {:unix, :freebsd} -> "gmake"
-      {:unix, :openbsd} -> "gmake"
-      {:unix, :netbsd} -> "gmake"
-      {:unix, :dragonfly} -> "gmake"
-      _ -> "make"
-    end
+    make_cmd =
+      System.get_env("MAKE") ||
+        case :os.type() do
+          {:unix, :freebsd} -> "gmake"
+          {:unix, :openbsd} -> "gmake"
+          {:unix, :netbsd} -> "gmake"
+          {:unix, :dragonfly} -> "gmake"
+          _ -> "make"
+        end
+
     {result, error_code} = System.cmd(make_cmd, [], stderr_to_stdout: true)
     IO.binwrite(result)
 
     if error_code != 0 do
-      raise Mix.Error, message: """
-        Could not run `#{make_cmd}`.
-        Please check if `make` and either `clang` or `gcc` are installed
-      """
+      raise Mix.Error,
+        message: """
+          Could not run `#{make_cmd}`.
+          Please check if `make` and either `clang` or `gcc` are installed
+        """
     end
 
-    Mix.Project.build_structure
+    Mix.Project.build_structure()
     :ok
   end
 end
@@ -107,6 +111,6 @@ defmodule Mix.Tasks.Spec do
   use Mix.Task
   @shortdoc "Runs spec test"
   def run(_) do
-    Mix.shell.cmd("make spec")
+    Mix.shell().cmd("make spec")
   end
 end
