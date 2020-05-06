5167d44c8792294884e52a5201261aa05b0487c8
Wojtek Mach <wojtek@wojtekmach.pl>
2020-05-06 17:13:04 +0200

    Remove config/
    We've dropped support for cmark in ExDoc since we now operate on AST and
    we don't have support for it here.

--- config/config.exs
+++
@@ -1,7 +0,0 @@
-# @deprecated; replace with `import Config`
-# once 1.9 is the last supported Elixir version
-use Mix.Config
-
-if Mix.env in ~w(dev docs ci)a do
-  config :ex_doc, :markdown_processor, ExDoc.Markdown.Cmark
-end
